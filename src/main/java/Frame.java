e02e049154336a36de12915b1ded550ab994a8b7
atushi <nakanoatushi413@gmail.com>
2021-10-11 01:05:44 +0900

    コードをメソッドにまとめました。 setLabel時にアニメーションをつけました。 (セッタでそういうことしていいのだろうか...)

--- src/main/java/Frame.java
+++ src/main/java/Frame.java
@@ -24,7 +24,7 @@ public class Frame extends JFrame {
         panel.setBackground(new Color(200, 200, 255, 255));
 
         layout.setConstraints(label, gbc);
-        label.setBackground(new Color(233,2, 2));
+        label.setBackground(new Color(255, 255, 255,255));
         label.setOpaque(true);
 
         panel.add(label);
@@ -38,6 +38,21 @@ public class Frame extends JFrame {
 
     public void setLabel(String text){
         label.setText("1BTC=" + text + "円");
+        Color defaultColor = label.getBackground();
+        Color blinkingColor = new Color(2, 200, 200,255);
+
+
+        //equals()じゃなくても動いた沼るからあんまり考えない方がいいかも...
+        Timer timer = new Timer(300 ,   e -> { label.setBackground((defaultColor.equals(label.getBackground())) ? blinkingColor : defaultColor);});
+        timer.start();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        timer.stop();
+        label.setBackground(defaultColor);
     }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,28 +10,31 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         Frame frame = new Frame(new Dimension(500, 500), "BTCチャート");
+        frame.setLabel(getPrice());
         frame.setVisible(true);
 
 
         for (; ; ) {
-
-            Document doc = null;
             try {
-                doc = Jsoup.connect("https://www.google.com/search?q=BTC").get();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            Elements elements = doc.select(".DFlfde.SwHCTb");
-            frame.setLabel(elements.get(0).text());
-
-            try {
-                Thread.sleep(1500);
+                Thread.sleep(15000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            frame.setLabel(getPrice());
 
         }
 
 
+    }
+
+    private static String getPrice() {
+        Document doc = null;
+        try {
+            doc = Jsoup.connect("https://www.google.com/search?q=BTC").get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Elements elements = doc.select(".DFlfde.SwHCTb");
+        return elements.get(0).text();
     }
 }
